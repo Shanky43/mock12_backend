0b69e9e2b676d7c7ea102de7bd44f0aa6ffa3678
Shankar S <112791993+Shanky43@users.noreply.github.com>
2023-06-13 15:09:30 +0530

    code

--- Routes/jobs.route.js
+++ Routes/jobs.route.js
@@ -2,25 +2,30 @@ const express = require("express");
 const JobModel = require("../Model/jobs.model");
 const JobsRouter = express.Router();
 
-
 JobsRouter.post("/postjobs", async (req, res) => {
-    try {
-        const postjobs = JobModel(req.body);
-        await postjobs.save();
-        res.status(200).send("Successfully added Data");
-    } catch (err) {
-        res.status(400).send({ err: err.message });
-    }
+  try {
+    const postjobs = JobModel(req.body);
+    await postjobs.save();
+    res.status(200).send("Successfully added Data");
+  } catch (err) {
+    res.status(400).send({ err: err.message });
+  }
 });
 
 JobsRouter.get("/", async (req, res) => {
-    try {
-        payload = req.body;
-        const viewjobs = await JobModel.find(payload);
-        res.status(200).send({ jobs: viewjobs });
-    } catch (err) {
-        res.status(400).send({ err: err.message });
+  try {
+    const { techstack } = req.query;
+    let query = {};
+
+    if (techstack) {
+      query.language = { $regex: techstack, $options: "i" };
     }
+
+    const viewjobs = await JobModel.find(query);
+    res.status(200).send({ jobs: viewjobs });
+  } catch (err) {
+    res.status(400).send({ err: err.message });
+  }
 });
 
-module.exports = { JobsRouter }
+module.exports = {JobsRouter};
